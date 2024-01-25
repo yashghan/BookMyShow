0ed5ae70224e454a5becb3206196bdf80abece36
yashghan <yashdghan@gmail.com>
2024-01-25 15:45:58 +0530

    Entity:Movie table creation

--- BookMyShow/src/main/java/com/example/BookMyShow/Entity/Movie.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/Entity/Movie.java
@@ -1,13 +1,36 @@
 package com.example.BookMyShow.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 
+@Table(name="Movie")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private int durInMinutes;
+    @Column(nullable = false)
+    private String language;
+    @Column(nullable = false)
+    private Date releaseDate;
+    @Column(nullable = false)
+    private String Country;
+    @Column(nullable = false)
+    private String genre;
 
 }
