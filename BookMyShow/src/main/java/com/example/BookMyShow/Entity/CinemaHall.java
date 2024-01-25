d66f92c7112f18a1e9d937fe270c852f20b7ee58
yashghan <yashdghan@gmail.com>
2024-01-25 16:04:12 +0530

    Entity:CinemaHall table creation

---
+++ BookMyShow/src/main/java/com/example/BookMyShow/Entity/CinemaHall.java
@@ -0,0 +1,26 @@
+package com.example.BookMyShow.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="Show")
+public class CinemaHall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long cinemaHallId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Long totalSeats;
+}
