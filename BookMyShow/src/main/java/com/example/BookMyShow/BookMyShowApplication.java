80ec2f92cf86eef3bd70f3caa0bec1011bc803fb
yashyenpure <yashyenpureyry1304@gmail.com>
2024-01-25 12:51:04 +0530

    hello world

--- BookMyShow/src/main/java/com/example/BookMyShow/BookMyShowApplication.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/BookMyShowApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookMyShowApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookMyShowApplication.class, args);
+
+	}
+
+	public void static String  hello(){
+		System.out.println("hello world");
 	}
 
 }
