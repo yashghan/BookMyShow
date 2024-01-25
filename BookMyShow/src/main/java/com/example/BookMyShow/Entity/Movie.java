c27dd3a84330ca7ece518e83fafbe95be9dffc7e
yashghan <yashdghan@gmail.com>
2024-01-25 15:29:41 +0530

    Entity:Movie changes and lombok addition

---
+++ BookMyShow/src/main/java/com/example/BookMyShow/Entity/Movie.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShow.Entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Movie {
+
+}
