7555ee14140b110a955176cd6d41ff18fcc8a727
yashyenpure <yashyenpureyry1304@gmail.com>
2024-01-29 12:51:40 +0530

    movie controller api update

--- BookMyShow/src/main/java/com/example/BookMyShow/Controller/MovieController.java
+++ BookMyShow/src/main/java/com/example/BookMyShow/Controller/MovieController.java
@@ -1,8 +1,35 @@
 package com.example.BookMyShow.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/")
 public class MovieController {
+    @GetMapping("/show")
+    public ResponseEntity<List<Show>> getAllShow(){
+        return movieService.fetchAllShows();
+    }
+    @GetMapping("/show/{id}")
+    public ResponseEntity<Optional<Show>> getProductById(@PathVariable Long id){
+        Optional<Show> show = movieService.fetchProductById(id).getBody();
+        if (show.isPresent()) {
+                return ResponseEntity.ok(show);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+
+
+
 
 }
